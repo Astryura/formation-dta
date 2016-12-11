9187615b1d77d1a04ed5177ada90ed05763fe04e
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2016-12-11 15:46:07 +0100

    javadoc

--- astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/FichierDaoFactory.java
+++ astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/FichierDaoFactory.java
@@ -2,8 +2,19 @@ package fr.pizzeria.dao;
 
 import fr.pizzeria.dao.pizza.PizzaDaoFichier;
 
+/**
+ * Classe fille de DaoFactory
+ * 
+ * @author Astrid Hlavacek
+ *
+ */
 public class FichierDaoFactory extends DaoFactory {
 
+	/**
+	 * instantiaction de PizzaDaoFichier
+	 * 
+	 * @see PizzaDaoFichier
+	 */
 	public FichierDaoFactory() {
 		super(new PizzaDaoFichier());
 	}
