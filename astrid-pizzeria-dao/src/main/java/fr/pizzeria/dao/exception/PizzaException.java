bb056abb2c76e7b7ba1b07e762f98d28a7121ae6
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2016-12-11 15:53:43 +0100

    javadoc

--- astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/exception/PizzaException.java
+++ astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/exception/PizzaException.java
@@ -1,19 +1,44 @@
 package fr.pizzeria.dao.exception;
 
+/**
+ * <h1>PizzaException</h1><b>Permet de gérer les exception runtime</b>
+ * 
+ * @author Astrid Hlavacek
+ *
+ */
 public class PizzaException extends RuntimeException {
 
+	/**
+	 * Constructeur sans parametre
+	 */
 	public PizzaException() {
 		super();
 	}
 
-	public PizzaException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
+	/**
+	 * Consqtructeur ayant pour parametre un string et une exception
+	 * 
+	 * @param msg
+	 * @param cause
+	 */
+	public PizzaException(String msg, Throwable cause) {
+		super(msg, cause);
 	}
 
-	public PizzaException(String arg0) {
-		super(arg0);
+	/**
+	 * Consqtructeur ayant pour parametre un string
+	 * 
+	 * @param msg
+	 */
+	public PizzaException(String msg) {
+		super(msg);
 	}
 
+	/**
+	 * Consqtructeur ayant pour parametre une exception
+	 * 
+	 * @param cause
+	 */
 	public PizzaException(Throwable cause) {
 		super(cause);
 	}
