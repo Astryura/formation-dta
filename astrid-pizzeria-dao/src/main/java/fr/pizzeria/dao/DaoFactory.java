47855d9ac22db581bc81b8cc4feee0f370fcb550
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2016-12-11 11:08:12 +0100

    javadoc

--- astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/DaoFactory.java
+++ astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/DaoFactory.java
@@ -2,15 +2,37 @@ package fr.pizzeria.dao;
 
 import fr.pizzeria.dao.pizza.PizzaDao;
 
+/**
+ * <h1>DaoFactory</h1> <b>Classe Abstraite sur le model du design pattern
+ * AbstractFactory, pour gérer le DAO</b>
+ * 
+ * @author Astrid Hlavacek
+ *
+ */
 public abstract class DaoFactory {
 
-	PizzaDao pizzaDao;
+	/**
+	 * Le PizzaDao
+	 * 
+	 * @see PizzaDao
+	 */
+	private PizzaDao pizzaDao;
 
+	/**
+	 * Constructeur instancie le PizzaDao
+	 * 
+	 * @param pizzaDao
+	 */
 	public DaoFactory(PizzaDao pizzaDao) {
 		super();
 		this.pizzaDao = pizzaDao;
 	}
 
+	/**
+	 * getPizzaDao(), retourne le PizzaDao
+	 * 
+	 * @return pizzaDao
+	 */
 	public PizzaDao getPizzaDao() {
 		return this.pizzaDao;
 	}
