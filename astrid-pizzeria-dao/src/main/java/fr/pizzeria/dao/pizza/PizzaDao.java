f5b6b1571c296871b9cfc511fbb5ceaafeac261d
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2016-12-11 16:02:18 +0100

    javadoc

--- astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/pizza/PizzaDao.java
+++ astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/pizza/PizzaDao.java
@@ -5,23 +5,73 @@ import java.util.List;
 import fr.pizzeria.dao.exception.PizzaException;
 import fr.pizzeria.model.Pizza;
 
+/**
+ * Interface définissant les actions possible d'une PizzaDao
+ * 
+ * @author Astrid Hlavacek
+ *
+ */
 public interface PizzaDao {
 
+	/**
+	 * retroune la liste de toutes les Pizzas
+	 * 
+	 * @return List<Pizza>
+	 * @throws PizzaException
+	 */
 	List<Pizza> findAllPizzas() throws PizzaException;
 
+	/**
+	 * Enregistre une nouvelle Pizza
+	 * 
+	 * @param pizza
+	 * @throws PizzaException
+	 */
 	void saveNewPizza(Pizza pizza) throws PizzaException;
 
+	/**
+	 * Modifie une Pizza
+	 * 
+	 * @param codePizza
+	 * @param pizza
+	 * @throws PizzaException
+	 */
 	void updatePizza(String codePizza, Pizza pizza) throws PizzaException;
 
+	/**
+	 * Supprime une Pizza
+	 * 
+	 * @param codePizza
+	 * @throws PizzaException
+	 */
 	void deletePizza(String codePizza) throws PizzaException;
 
+	/**
+	 * retourne la liste de Pizza par ordre de catégorie
+	 * 
+	 * @return List<Pizza>
+	 */
 	List<Pizza> findAllPizzasCat();
 
+	/**
+	 * Trouve la Pizza la plus chère
+	 * 
+	 * @return Pizza
+	 */
 	Pizza findPrixMaxPizza();
 
+	/**
+	 * Import les données fichiers ou Tableaux en BDD (PizzaDaoJDBC)
+	 * 
+	 * @throws PizzaException
+	 * @see PizzaDaoJDBC
+	 */
 	default void importDataPizza() throws PizzaException {
 		throw new PizzaException("pas d'implémentation");
 	}
 
+	/**
+	 * Ferme le PizzaDao
+	 */
 	void close();
 }
