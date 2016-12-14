e733f12caa4e893bcd23a93f5b2b2d31cb9839f7
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2016-12-14 16:05:02 +0100

    jpaLivreur

--- astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/livreur/LivreurDao.java
+++ astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/livreur/LivreurDao.java
@@ -1,10 +1,11 @@
 package fr.pizzeria.dao.livreur;
 
+import fr.pizzeria.dao.JPADao;
 import fr.pizzeria.model.Livreur;
 
 public interface LivreurDao {
 
-	void AddLivreur(Livreur livreur);
+	void AddLivreur(Livreur livreur, JPADao jpaDao);
 
 	/**
 	 * 
--- astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/livreur/LivreurDaoJPA.java
+++ astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/livreur/LivreurDaoJPA.java
@@ -1,52 +1,15 @@
 package fr.pizzeria.dao.livreur;
 
-import java.util.logging.Logger;
-
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceException;
 
-import fr.pizzeria.dao.IRunJPA;
-import fr.pizzeria.dao.exception.PizzaException;
-import fr.pizzeria.dao.pizza.PizzaDaoJPA;
+import fr.pizzeria.dao.JPADao;
 import fr.pizzeria.model.Livreur;
 
 public class LivreurDaoJPA implements LivreurDao {
 
-	private EntityManagerFactory emfactory;
-
-	/**
-	 * Constructeur instanciant le EntityManagerFactory
-	 */
-	public LivreurDaoJPA() {
-		emfactory = Persistence.createEntityManagerFactory("pizzeria-console");
-	}
-
-	/**
-	 * 
-	 * @param run
-	 * @return T
-	 */
-	public <T> T execute(IRunJPA<T> run) {
-		EntityManager entitymanager = null;
-		try {
-			entitymanager = emfactory.createEntityManager();
-			return run.exec(entitymanager);
-		} catch (PersistenceException e) {
-			Logger.getLogger(PizzaDaoJPA.class.getName()).severe(e.getMessage());
-			throw new PizzaException(e);
-		} finally {
-			if (entitymanager != null) {
-				entitymanager.close();
-			}
-		}
-
-	}
-
 	@Override
-	public void AddLivreur(Livreur livreur) {
-		execute((EntityManager entitymanager) -> {
+	public void AddLivreur(Livreur livreur, JPADao jpaDao) {
+		jpaDao.execute((EntityManager entitymanager) -> {
 			entitymanager.getTransaction().begin();
 			entitymanager.persist(livreur);
 			entitymanager.getTransaction().commit();
@@ -57,6 +20,6 @@ public class LivreurDaoJPA implements LivreurDao {
 
 	@Override
 	public void close() {
-		emfactory.close();
+
 	}
 }
