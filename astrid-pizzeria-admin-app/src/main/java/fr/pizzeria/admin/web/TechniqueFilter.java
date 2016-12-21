6c1f2972014d995d9ee9504ce5ef4d849a1f329b
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2016-12-21 17:44:03 +0100

    override

--- astrid-pizzeria-admin-app/src/main/java/fr/pizzeria/admin/web/TechniqueFilter.java
+++ astrid-pizzeria-admin-app/src/main/java/fr/pizzeria/admin/web/TechniqueFilter.java
@@ -19,14 +19,17 @@ public class TechniqueFilter implements Filter {
 
 	private FilterConfig config = null;
 
+	@Override
 	public void init(FilterConfig config) throws ServletException {
 		this.config = config;
 	}
 
+	@Override
 	public void destroy() {
 		config = null;
 	}
 
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		long before = System.currentTimeMillis();
