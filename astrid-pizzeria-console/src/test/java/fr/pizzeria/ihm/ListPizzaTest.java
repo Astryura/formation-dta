25311945006be0f21473dcec3c8be916a6359691
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2017-01-05 11:19:02 +0100

    up

--- astrid-pizzeria-console/src/test/java/fr/pizzeria/ihm/ListPizzaTest.java
+++ astrid-pizzeria-console/src/test/java/fr/pizzeria/ihm/ListPizzaTest.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import fr.pizzeria.console.PizzeriaAppSpringConfig;
+import fr.pizzeria.console.PizzeriaAppSpringConfigTest;
 import junit.framework.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = PizzeriaAppSpringConfig.class)
+@ContextConfiguration(classes = PizzeriaAppSpringConfigTest.class)
 public class ListPizzaTest {
 
 	@Autowired
