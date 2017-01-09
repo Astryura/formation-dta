9c6c948d311351b36e0f21e5447fb6cf0fd01465
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2017-01-09 18:08:55 +0100

    up

--- astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/other/MaClasseAspect.java
+++ astrid-pizzeria-dao/src/main/java/fr/pizzeria/dao/other/MaClasseAspect.java
@@ -39,7 +39,7 @@ public class MaClasseAspect {
 		String today = dateFormat.format(date);
 		long before = System.currentTimeMillis();
 		final Object[] args = pjp.getArgs();
-		StringBuffer sb = new StringBuffer();
+		final StringBuilder sb = new StringBuilder();
 		sb.append(pjp.getSignature().getName());
 		if (args.length != 0) {
 			sb.append(" avec les parametres : (");
